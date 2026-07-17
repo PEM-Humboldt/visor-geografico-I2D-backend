dbd0a3a78668f875c83ca30fbbbf32885889ec71
daniel4nnah <laura.svilla21@gmail.com>
2026-07-17 09:59:34 -0500

    Optimice zip_generation script to not depend on codes.json

--- applications/gbif/management/commands/zip_generation.py
+++ applications/gbif/management/commands/zip_generation.py
@@ -11,45 +11,62 @@ class Command(BaseCommand):
         def now():
             return f"[{timezone.now().strftime('%Y-%m-%d %H:%M:%S')}] "
         
-        codigos_file = os.path.join(settings.MEDIA_ROOT, 'codes.json')
-
-        if os.path.exists(codigos_file):
-            try:
-                with open(codigos_file, 'r') as f:
-                    codigos = json.load(f)
-                    mpios_dict = codigos.get('mpios', {})
-                    dptos_dict = codigos.get('dptos', {})
-            except json.JSONDecodeError:
-                self.stdout.write(self.style.ERROR(f"{now()} - Error parsing JSON."))
-                return
-        else:
-            self.stdout.write(self.style.ERROR(f"{now()} - File not found. Skipping."))
-            return
-
-
+        query = """
+            SELECT 
+                m.codigo AS mpio_codigo,
+                m.nombre AS mpio_nombre,
+                d.codigo AS dpto_codigo,
+                d.nombre AS dpto_nombre
+            FROM capas_base.mpio_politico m
+            INNER JOIN dpto_politico d ON LEFT(m.codigo, 2) = d.codigo
+            ORDER BY dpto_codigo;
+        """
+        
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            # Fetchall returns a list of tuples, we convert them to dicts for ease of use
+            columns = [col[0] for col in cursor.description]
+            codes = [
+                dict(zip(columns, row))
+                for row in cursor.fetchall()
+            ]
+        
         output_dir = os.path.join(settings.MEDIA_ROOT, 'cached_zips')
         os.makedirs(output_dir, exist_ok=True)
 
         stats = {"success": 0, "failed": 0}
+        zipped_dptos = []
 
-        for codigo, nombre in mpios_dict.items():
+        for code in codes:
+
+            # Generate mpios zip
             try:
-                self.stdout.write(f"Generating ZIP at {now()} for {nombre} - {codigo}...")
-                generar_zip(codigo, 'codigo_mpio', nombre)
+                mpio_code = code['mpio_codigo']
+                mpio_name = code['mpio_nombre']
+
+                self.stdout.write(f"Generating ZIP at {now()} for MPIO {mpio_name} - {mpio_code}...")
+                #generar_zip(mpio_code, 'codigo_mpio', mpio_name)
                 stats["success"] += 1
             except Exception as e:
                 stats["failed"] += 1
-                self.stdout.write(self.style.ERROR(f"FAILED at {now()} - ERROR generating ZIP for Municipio: {nombre} ({codigo}). Error: {str(e)}"))
+                self.stdout.write(self.style.ERROR(f"FAILED at {now()} - ERROR generating ZIP for Municipio: {mpio_name} - {mpio_code}. Error: {str(e)}"))
                 self.stdout.write(traceback.format_exc())
 
-        for codigo, nombre in dptos_dict.items():
-            try:
-                self.stdout.write(f"Generating ZIP at {now()} for {nombre} - {codigo}...")
-                generar_zip(codigo, 'codigo_dpto', nombre)
-                stats["success"] += 1
-            except Exception as e:
-                stats["failed"] += 1
-                self.stdout.write(self.style.ERROR(f"FAILED at {now()} - ERROR generating ZIP for Departamento: {nombre} ({codigo}). Error: {str(e)}"))
-                self.stdout.write(traceback.format_exc())
+            # Generate dptos zip only if it hasn't been generated already
+            dpto_code = code['dpto_codigo']
+            if dpto_code not in zipped_dptos:
+                try:
+                    dpto_name = code['dpto_nombre']
+
+                    self.stdout.write(f"============== Generating ZIP at {now()} for DPTO {dpto_name} - {dpto_code}...")
+                    #generar_zip(dpto_code, 'codigo_dpto', dpto_name)
+                    stats["success"] += 1
+                    zipped_dptos.append(dpto_code)
+                except Exception as e:
+                    stats["failed"] += 1
+                    self.stdout.write(self.style.ERROR(f"FAILED at {now()} - ERROR generating ZIP for Department: {dpto_name} - {dpto_code}. Error: {str(e)}"))
+                    self.stdout.write(traceback.format_exc())
+
+        print("Dptos generated: ", len(zipped_dptos))
 
         self.stdout.write(self.style.SUCCESS(f"ZIP generation completed at {now()}. Summary: {stats['success']} succeeded, {stats['failed']} failed."))
